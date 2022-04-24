b08097cc65bed9cdca8e86948a876d566f7b414b
James Perih <james@hotdang.ca>
2022-04-23 23:47:32 -0600

    Call to action button.

--- lib/widgets/main_nav.dart
+++ lib/widgets/main_nav.dart
@@ -1,8 +1,9 @@
-import 'package:blog/widgets/main_nav_section_menu.dart';
+import 'package:blog/config/strings.dart';
 import 'package:flutter/material.dart';
 
+import 'main_nav_section_menu.dart';
+import 'main_nav_call_to_action.dart';
 import 'main_nav_logo.dart';
-import 'main_nav_section_menu_item.dart';
 
 class MainNavigation extends StatelessWidget {
   const MainNavigation({Key? key}) : super(key: key);
@@ -16,6 +17,12 @@ class MainNavigation extends StatelessWidget {
         children: <Widget>[
           const MainNavigationLogo(),
           MainNavigationSectionMenu(),
+          MainNavigationCallToAction(
+            callToActionLabel: Strings.callToActionLabel,
+            callToActionHandler: () {
+              // TODO: do a thing!
+            },
+          ),
         ],
       ),
     );
